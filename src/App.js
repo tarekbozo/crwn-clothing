08e420af650a900c25dd47994520279298e0bb16
Your Name <you@example.com>
2019-08-08 00:22:25 +0200

    created project and added homepahe file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './styles/homepage.style.scss'
+import Homepage from './components/homepage.component';
+
+
+function App() {
+  return (
+    <div >
+    < Homepage />
+    </div>
+  );
+}
+
+export default App;
