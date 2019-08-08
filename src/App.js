42a758528ea471ca54ea5faa97ff45ce2b4c3b79
Your Name <you@example.com>
2019-08-08 21:04:28 +0200

    broken homepage into smaller components, finished styling menu item componenet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import './styles/homepage.style.scss'
-import Homepage from './components/homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 
 function App() {
